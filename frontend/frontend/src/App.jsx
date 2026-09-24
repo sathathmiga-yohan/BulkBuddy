bb9eb20d356e27b07bfa043c6b88fb72c1e6232b
santhanaluxmy <santhanaluxmy20@gmail.com>
2026-09-24 11:50:55 +0530

    chore: setup React frontend

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container py-5">
+        <h1>BulkBuddy</h1>
+        <p>Buy Together. Save More.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
